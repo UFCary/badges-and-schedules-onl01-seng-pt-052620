37521b264af902a1245a5f56daf3f846fb786d74
Cary Williams <cary.l.williams00@gmail.com>
2020-06-02 01:49:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end  
+ 
+ def batch_badge_creator(attendees)
+   new_msgs = []
+   badges.map do |names|
+     new_msgs << badges
+    end
+  end
+     
+ def assign_rooms(room_assignments)
+   room_assignments.each_with_index
+ end 
+  
+
+
+
